359f842ea8a9dd1f7d6c77018833903b8c26f053
wobless <wobless@runchance.com>
2026-07-13 10:40:05 +0800

    修复windows项目内启动路径

--- windows.bat
+++ windows.bat
@@ -6,7 +6,7 @@ cd /d "%~dp0"
 
 set "PHP_BIN=php"
 if exist "%~dp0php.exe" set "PHP_BIN=%~dp0php.exe"
-if exist "%~dp0php85\php.exe" set "PHP_BIN=%~dp0php85\php.exe"
+if exist "%~dp0php\php.exe" set "PHP_BIN=%~dp0php\php.exe"
 
 "%PHP_BIN%" windows.php start
 if errorlevel 1 (
